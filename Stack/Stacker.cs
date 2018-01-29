97b3bae980ceb16190d9330e8e39ce36683c5462
maxma <max.may@live.de>
2018-01-29 14:41:53 +0100

    Projektdateien hinzufügen.

---
+++ Stack/Stacker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Stack
+{
+    public class Stacker<T>
+    {
+
+        List<T> list = new List<T>();
+
+        public bool IsEmpty()
+        {
+            return list.Any();
+        }
+
+        public void Push(T obj)
+        {
+            list.Add(obj);
+        }
+
+        public T Pop()
+        {
+            return list.First();
+        } 
+
+
+    }
+}
---
+++ TestStack/TestStacker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Stack;
+
+namespace TestStack
+{ 
+    [TestFixture]
+    class TestStacker
+    {
+
+        [Test]
+        public void TestIsEmpty()
+        {
+            Stack.Stacker<String> s = new Stack.Stacker<String>();
+            Assert.True(s.IsEmpty());
+            s.Push("Object1");
+            Assert.False(s.IsEmpty());
+        }
+
+    }
+}
